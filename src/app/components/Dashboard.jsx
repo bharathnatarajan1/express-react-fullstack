cef27f67301cff34ede9130dcd68e17543a39772
Bharath Natarajan <bharath_eep@yahoo.com>
2020-06-14 20:15:22 +0530

    Updated with Redux Code for application state

---
+++ src/app/components/Dashboard.jsx
@@ -0,0 +1,13 @@
+/**
+ * The dashboard is a simple React component that contains several lists of tasks,
+ * one for each group that belongs to the user.
+ */
+
+import React from 'react';
+
+
+export const Dashboard = ({groups})=>(
+    <div>
+        <h2>Dashboard</h2>
+    </div>
+)
--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,15 @@
 console.log("Hello World!");
+
+//import { store } from './store'
+
+//console.log(store.getState() );
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { Dashboard } from './components/Dashboard'
+
+
+ReactDOM.render(
+     <Dashboard/>,
+)
