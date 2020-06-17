14f51f310d79b2eac65af846e68cd5538df5bcf5
Bharath Natarajan <bharath_eep@yahoo.com>
2020-06-17 21:44:30 +0530

    Updated src/app/index.jsx  code.

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,8 +1,8 @@
 console.log("Hello World!");
 
-//import { store } from './store'
+import { store } from './store'
 
-//console.log(store.getState() );
+console.log(store.getState() );
 
 import React from 'react';
 import ReactDOM from 'react-dom';
