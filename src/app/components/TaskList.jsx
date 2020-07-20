3738c291bea51dc1e74e02ce271458b7cfcea6e4
Bharath Natarajan <bharath_eep@yahoo.com>
2020-07-20 23:14:00 +0530

    Updated code with Saga middleware implementation.

--- src/app/components/TaskList.jsx
+++ src/app/components/TaskList.jsx
@@ -1,16 +1,19 @@
 
 import React from 'react';
 import { connect } from 'react-redux';
-// import { requestTaskCreation } from '../store/mutations'
+import { requestTaskCreation } from '../store/mutations'
 // import { ConnectedTaskListItem } from './TaskListItem'
 
-export const TaskList = ({tasks, name})=>(
+export const TaskList = ({tasks, name, id, createNewTask})=>(
     <div>
         <h3>
            {name}
         </h3>
         <div>
-            {tasks.map(task=> (<div>{task.name}</div>))}
+            {tasks.map(task=> (<div key={task.id}>{task.name}{task.id}</div>))}
+        </div>
+        <div>
+            <button onClick={()=>createNewTask(id)}>Add New</button>
         </div>
     </div>
 )
@@ -24,4 +27,19 @@ const mapStateToProps = (state, ownProps)=>{
     }
 };
 
-export const ConnectedTaskList = connect(mapStateToProps)(TaskList);
+// const mapDispatchToProps = (dispatch, ownProps)=>{
+//     return {
+//       createNewTask(id) {
+//         console.log("Creating new task...", id);
+//         dispatch(requestTaskCreation(id));
+//       }
+//     }
+// }
+
+const mapDispatchToProps = (dispatch, {id})=>({
+    createNewTask(){
+        dispatch(requestTaskCreation(id));
+    }
+});
+
+export const ConnectedTaskList = connect(mapStateToProps, mapDispatchToProps)(TaskList);
